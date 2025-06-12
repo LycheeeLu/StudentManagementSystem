da285589e2bd314542a50757457e6a447b4cce49
Jie Lu <jie.lu@tuni.fi>
2025-06-12 20:37:57 +0800

    Add: sql session util

---
+++ src/main/java/com/it/util/MyBatisUtil.java
@@ -0,0 +1,34 @@
+package com.it.util;
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Package : com.it.util
+ * @ClassName : MyBatisUtil.java
+ * @Version : 1.0
+ * @Description :
+ */
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisUtil {
+    private static SqlSessionFactory sqlSessionFactory;
+    static {
+        try{
+            String resource = "mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSession getSession() {
+        return sqlSessionFactory.openSession();
+    }
+}
